0e78986287441d4be391819b83f8658bd735989e
Slimtouflo <slimtouflo@.com>
2024-02-15 14:10:46 +0100

    Add categories.js and sounds.js

---
+++ src/sounds.js
@@ -0,0 +1,64 @@
+const sounds = [
+    {
+        id: "PREOC",
+        name: "Préocupation",
+        duration: 96,
+        image: "https://api.deepai.org/job-view-file/6df6fd96-e0c5-475a-b55b-5ff1d9cfb9f5/outputs/output.jpg"
+    },
+    {
+        id: "EXPLO",
+        name: "Exploration",
+        duration: 96,
+        image: "https://api.deepai.org/job-view-file/4aae30e1-c384-4538-941b-b7a98fbbfeb0/outputs/output.jpg"
+    },
+    {
+        id: "FIGHT",
+        name: "Combat",
+        duration: 94,
+        image: "https://api.deepai.org/job-view-file/1f0a4fa7-e56f-46c9-8a09-4dd56fde5501/outputs/output.jpg"
+    },
+    {
+        id: "JOY",
+        name: "Joie",
+        duration: 94,
+        image: "https://api.deepai.org/job-view-file/2673e794-f175-4219-85f4-a4af34c203e3/outputs/output.jpg"
+    },
+    {
+        id: "NOTH",
+        name: "Néant",
+        duration: 95,
+        image: "https://api.deepai.org/job-view-file/48428706-3933-4d31-ae61-12d5104bb7cb/outputs/output.jpg"
+    },
+    {
+        id: "MEMO",
+        name: "Mémoire",
+        duration: 95,
+        image: "https://api.deepai.org/job-view-file/ace20dab-ff41-46ca-81b1-2ae3625d4b68/outputs/output.jpg"
+    },
+    {
+        id: "SAD",
+        name: "Tristesse",
+        duration: 96,
+        image: "https://api.deepai.org/job-view-file/4ee9741b-bab3-424f-a887-d3d8301d2993/outputs/output.jpg"
+    },
+    {
+        id: "WOND",
+        name: "Merveille",
+        duration: 96,
+        image: "https://api.deepai.org/job-view-file/527b9039-96c4-421e-9b07-9d1a79b56ecb/outputs/output.jpg"
+    },
+    {
+        id: "WOND2",
+        name: "Emerveillement",
+        duration: 93,
+        image: "https://api.deepai.org/job-view-file/0f36af45-ed60-4c16-b6d6-b681ea019852/outputs/output.jpg"
+    },
+    {
+        id: "WORK",
+        name: "Réflexion",
+        duration: 92,
+        image: "https://api.deepai.org/job-view-file/66bd90ce-0d67-497c-b4ed-344b551b4b86/outputs/output.jpg"
+    }
+];
+
+export default sounds;
